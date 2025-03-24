faa96f3c28e84c5ef7382cd79d91f9a511596d65
abdulaliarafat <abdulaliarafat@gmail.com>
2025-03-24 19:14:33 +0600

    react work

--- explore-react/src/App.jsx
+++ explore-react/src/App.jsx
@@ -6,14 +6,58 @@ function App() {
   return (
     <>
       <h1>React Coure concept</h1>
+      <Students></Students>
+      <Students></Students>
+      <Person></Person>
+      <Developer name='Arafat' tech='jS'></Developer>
+      <Developer name='Rahat' tech='jS'></Developer>
+      <Developer name='Refat' tech='jS'></Developer>
+      <Player name='tamim' runs='5000'></Player>
+      <Player name='Musfiqur' runs='5000'></Player>
+      
     </>
   )
 }
+function Player({name,runs}){
+  return(
+     <div className='students'>
+      <h3>Name: {name}</h3>
+      <p>Run: {runs}</p>
+     </div>
+  )
+}
+function Students(){
+  return(
+   <div className='students'>
+    <p>Name:</p>
+    <p>Dept:</p>
+   </div>
+  )
+}
+function Developer(props){
+  console.log(props)
+  return(
+    <div style={{
+      border:'2px solid blue',
+      borderRadius:'20px'
+    }}>
+      <h3>Developer: {props.name} </h3>
+      <p>Tecnology: {props.tech
+        }</p>
+    </div>
+  )
+}
+
+
 function Person(){
   const age = 17;
   const name = "jolil"
+  const personStyle={
+    color:"red",
+    textAlign:'center'
+  }
   return(
-    <p>I am Person {name} {age}</p>
+    <p style={personStyle}>I am Person {name} {age}</p>
   )
 }
 function Criket(){
