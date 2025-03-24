def3cc6ecb1d40cb0524b94409a8f00bf291afe1
abdulaliarafat <abdulaliarafat@gmail.com>
2025-03-24 16:04:07 +0600

    connect repo of react

---
+++ explore-react/src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>React Coure concept</h1>
+    </>
+  )
+}
+function Person(){
+  const age = 17;
+  const name = "jolil"
+  return(
+    <p>I am Person {name} {age}</p>
+  )
+}
+function Criket(){
+  return(
+    <div>
+      <h1>Criket</h1>
+      <p>Play and loseing</p>
+    </div>
+  )
+}
+
+export default App
