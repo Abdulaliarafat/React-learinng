30102cf0c3bf19e780823b62c103d2aa1c6d7cbb
abdulaliarafat <abdulaliarafat@gmail.com>
2025-04-01 20:58:21 +0600

    react work rest countries

--- react-world-on-the-go/src/components/countries/countries.jsx
+++ react-world-on-the-go/src/components/countries/countries.jsx
@@ -1,14 +1,17 @@
 import React, { use } from 'react';
 import Country from '../country/Country';
+import './countries.css'
 
 const Countries = ({countriesPromise}) => {
   const countries= use(countriesPromise)
     return (
         <div>
             <h2>Travaling countries : {countries.length}</h2>
-            {
+           <div  className='countries'>
+           {
                 countries.map(country=><Country key={country.cca3} country={country}></Country>)
             }
+           </div>
         </div>
     );
 };
--- react-world-on-the-go/src/components/country/Country.jsx
+++ react-world-on-the-go/src/components/country/Country.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import'./country.css'
 const Country = ({country}) => {
     console.log(country)
     return (
-        <div>
+        <div className='country'>
             <h4>Name : {country.name.common}</h4>
             <img src={country.flags.png} alt="" />
             <p>Indepandent : {country.independent ? 'Freedom' : 'Ruleled'}</p>
