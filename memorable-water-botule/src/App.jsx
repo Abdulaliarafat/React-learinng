d5325150fee2e6ceb0481c3eed0eeeef24e42770
abdulaliarafat <abdulaliarafat@gmail.com>
2025-04-03 15:48:45 +0600

    projet water bottle

--- memorable-water-botule/src/App.jsx
+++ memorable-water-botule/src/App.jsx
@@ -1,12 +1,26 @@
+import { Suspense } from 'react'
 import './App.css'
+import Bottles from './components/Bottoles/Bottles'
+
+const bottlesPromise= fetch('./bottle.json').then(res=>res.json());
+const bottlesPromise2 = fetch('https://raw.githubusercontent.com/Abdulaliarafat/bottles-data/refs/heads/main/bottles.json').then(res=>res.json())
 
 function App() {
+   
+  // const bottle=[
+  //   {id:1, name:'pink bottle',price:250,color:'pink'},
+  //   {id:2, name:'pink bottle',price:250,color:'pink'},
+  //   {id:3, name:'pink bottle',price:250,color:'pink'},
+  //   {id:4, name:'pink bottle',price:250,color:'pink'},
+  // ]
 
   return (
     <>
 
       <h2>Buy awsome water bottle</h2>
-     
+     <Suspense fallback={<h3>Bottles are loading.............</h3>}>
+      <Bottles bottlesPromise={bottlesPromise}></Bottles>
+     </Suspense>
     </>
   )
 }
