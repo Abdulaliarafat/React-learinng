61241a23ff6a988b34b076d0085d44e13aae9e37
abdulaliarafat <abdulaliarafat@gmail.com>
2025-04-15 08:21:40 +0600

    explore react router

--- react-ruter-into/src/main.jsx
+++ react-ruter-into/src/main.jsx
@@ -1,18 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
+import { createBrowserRouter, RouterProvider,  } from 'react-router'
+import Root from './components/Root/Root'
+import Home from './components/home/Home'
+import Mobiles from './components/Mobiles/Mobiles'
+import Laptops from './components/Mobiles/Laptops/Laptops'
+
+
+
 const router=createBrowserRouter([
   {
-    path:"/",
-    element:<div>Hello from react router</div>,
-  }
+    path:'/',
+    Component: Root,
+    children:[
+      {index:true,Component:Home},
+      {path:'Mobiles',Component:Mobiles},
+      {path:'Laptops',Component:Laptops}
+    ]
+  },
+   {
+    path:'/about',
+    element:<div>about me</div>
+   },
+   {
+    path:'/blog',
+    element:<div>blog</div>
+   },
+   
 ])
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}></RouterProvider>
